1cad74a6051ac0a7c15012d49f0f09767e6cbb26
unregistered436 <patrick@themcneils.org>
2023-12-07 16:40:21 -0500

    Update AssemblyInfo.cs

--- Verademo-Dotnet/Properties/AssemblyInfo.cs
+++ Verademo-Dotnet/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+//test
 [assembly: AssemblyTitle("Verademo_dotnet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
